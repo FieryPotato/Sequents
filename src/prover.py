7cb3816fd46e3f7262c30f9d77f8459bcaa1795b
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-08-07 21:25:33 -0400

    "progress on prover io"
    	modified:   src/prover.py
        new file:   test/.test_prover.py.swp
    	modified:   test/test_prover.py

--- src/prover.py
+++ src/prover.py
@@ -1,16 +1,25 @@
 class Prover:
     def __init__(self) -> None:
-        self.import_filename: str | None = None
-        self.imported_contents: list[str] | None = None
+        self.infile: str | None = None
+        self.contents: list[str] | None = None
         self.outfile: str | None = None
-        
 
-    def _import(self, path: str, outfile=None) -> None:
-        self.import_filename = path
-        with open(path, "r") as file:
-            self.imported_contents = file.readlines()
+    def import_(self, path: str, outfile=None) -> None:
+        '''
+        Import contents of file to self and sets output file.
+        '''
+        self.infile = path
+        with open(path, 'r') as file:
+            self.contents = file.readlines()
         if outfile is None:
-            self.outfile = "".join(path.split(".")[:-1]) + "_result.json"
+            self.outfile = ''.join(path.split('.')[:-1]) + '_result.json'
         else:
             self.outfile = outfile
+
+    def export_(self) -> None:
+        '''
+        Export self.contents to the outfile.
+        '''
+        with open(self.outfile, 'w') as file:
+            file.write(str(self.contents))
 
--- test/test_prover.py
+++ test/test_prover.py
@@ -4,36 +4,54 @@ import unittest
 from src.prover import Prover
 
 
-EMPTY_TEST = "test/io_testing/empty_test.txt"
-TEST_FILE = "test/io_testing/test_file.txt"
+EMPTY_TEST = 'test/io_testing/empty_test.txt'
+TEST_FILE = 'test/io_testing/test_file.txt'
 
-class TestProver(unittest.TestCase):
+class TestProverIO(unittest.TestCase):
     def setUp(self) -> None:
+        self.test_out = 'test_out.txt'
         self.prover = Prover()
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.test_out):
+            os.remove(self.test_out)
 
     def test_get_import_file_contents(self) -> None:
         for file in (EMPTY_TEST, TEST_FILE):
-            self.prover._import(file)
+            self.prover.import_(file)
             with open(file) as f:
                 expected_contents = f.readlines()
-            self.assertEqual(expected_contents, self.prover.imported_contents)
+            self.assertEqual(expected_contents, self.prover.contents)
 
-    def test_get_import_filename(self) -> None:
-        self.prover._import(EMPTY_TEST)
-        self.assertEqual(EMPTY_TEST, self.prover.import_filename)
+    def test_get_infile(self) -> None:
+        self.prover.import_(EMPTY_TEST)
+        self.assertEqual(EMPTY_TEST, self.prover.infile)
 
     def test_get_output_filename_during_import(self) -> None:
-        suffix = "_result.json"
+        suffix = '_result.json'
         out = EMPTY_TEST[:-4] + suffix
-        self.prover._import(EMPTY_TEST)
+        self.prover.import_(EMPTY_TEST)
         self.assertEqual(out, self.prover.outfile)
     
-    def test_custom_outfile_during_import(self) -> None:
-        custom_out = "results.json"
-        self.prover._import(EMPTY_TEST, outfile=custom_out)
+    def test_custom_outfile_duringimport_(self) -> None:
+        custom_out = 'results.json'
+        self.prover.import_(EMPTY_TEST, outfile=custom_out)
         self.assertEqual(custom_out, self.prover.outfile)
 
+    def test_save_file_to_correct_output(self) -> None:
+        self.prover.outfile = self.test_out
+        self.prover.export_()
+        self.assertTrue(os.path.exists(self.test_out))
 
-if __name__ == "__main__":
+    def test_outfile_saves_prover_content(self) -> None:
+        out_contents = "Testing output contents."
+        self.prover.outfile = self.test_out
+        self.prover.contents = out_contents
+        self.prover.export_()
+        with open(self.test_out, 'r') as file:
+            self.assertEqual(file.read(), self.prover.contents)
+
+
+if __name__ == '__main__':
     unittest.main()
 
