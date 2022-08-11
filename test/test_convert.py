ced88dc3772d8e69cf207aff5743faa5e6345127
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-08-11 11:30:30 -0400

    Moved Convert class operations into Proposition as class methods and static methods.

--- test/test_convert.py
+++ test/test_convert.py
@@ -1,10 +1,9 @@
 import unittest
 
-from src.convert import Convert
 from src.proposition import Atom, Conditional, Conjunction,\
-        Disjunction, Negation
+        Disjunction, Negation, Proposition
 
-class TestConvert(unittest.TestCase):
+class TestConversion(unittest.TestCase):
     test_words = ['the grass is green', 'the birds are singing']
     conjunction = Conjunction(Atom('the grass is green'), Atom('the birds are singing'))   
     conditional = Conditional(Atom('the grass is green'), Atom('the birds are singing'))
@@ -13,43 +12,43 @@ class TestConvert(unittest.TestCase):
     def test_atom(self):
         string = 'The cat is on the mat'
         expected = Atom('The cat is on the mat')
-        actual = Convert(string).to_proposition()
+        actual = Proposition.from_string(string)
         self.assertEqual(expected, actual)
 
     def test_conjunction_words_simple_no_parens(self):
         string = ' and '.join(self.test_words)
         expected = self.conjunction     
-        actual = Convert(string).to_proposition()
+        actual = Proposition.from_string(string)
         self.assertEqual(expected, actual)
 
     def test_conjunction_symb_simple_no_parens(self):
         string = ' & '.join(self.test_words)
         expected = self.conjunction
-        actual = Convert(string).to_proposition()
+        actual = Proposition.from_string(string)
         self.assertEqual(expected, actual)
 
     def test_conditional_words_simple_no_parens(self):
         string = 'if {} then {}'.format(*self.test_words)
         expected = self.conditional
-        actual = Convert(string).to_proposition()
+        actual = Proposition.from_string(string)
         self.assertEqual(expected, actual)
 
     def test_conditional_symb_simple_no_parens(self):
         string = ' -> '.join(self.test_words)
         expected = self.conditional
-        actual = Convert(string).to_proposition()
+        actual = Proposition.from_string(string)
         self.assertEqual(expected, actual)
 
     def test_disjunction_words_simple_no_parens(self):
         string = ' or '.join(self.test_words)
         expected = self.disjunction
-        actual = Convert(string).to_proposition()
+        actual = Proposition.from_string(string)
         self.assertEqual(expected, actual)
 
     def test_disjunction_symb_simple_no_parens(self):
         string = ' v '.join(self.test_words)
         expected = self.disjunction
-        actual = Convert(string).to_proposition()
+        actual = Proposition.from_string(string)
         self.assertEqual(expected, actual)
 
 
