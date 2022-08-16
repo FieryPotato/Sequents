d26b63222d4623da7d2b9b36cbb316164123cf0e
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-08-16 12:32:30 -0400

    Revert "Revert "Made propositions immutable dataclasses with slots.""
    This reverts commit 9cb5cb6b6be35eb63bdbbccd6721eec973e2cc83.
    
    	modified:   src/proposition.py
    	modified:   test/test_io.py
    	modified:   test/test_proposition.py

--- src/proposition.py
+++ src/proposition.py
@@ -1,52 +1,27 @@
 from abc import ABC, abstractmethod
 from collections import namedtuple
+from dataclasses import dataclass
 
 SIDES: set[str] = {'ant', 'con'}
 
 tupseq = namedtuple('tupseq', ['ant', 'con'])
 
-
+@dataclass(frozen=True, order=True)
 class Proposition(ABC): 
     """
     Base class for propositions.
     """
 
-    def __init__(self, *args):
-        self.content = [arg for arg in args]
-        if len(self.content) != self.arity:
-            raise ValueError(f'A {self.__class__} contains exactly '\
-                             f'{self.arity} propositions.')
- 
+    def __post_init__(self) -> None:
+        self.validate_content()
+
+    @abstractmethod
+    def content(self) -> list:
+        """Return this object's propositional content."""
+    
     @property
     def complexity(self) -> int:
         return 1 + max(p.complexity for p in self.content) 
-    
-    def __str__(self) -> str:
-        return self.__repr__()
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.content})'
-
-    def __eq__(self, other) -> bool:
-        return self.__class__ == other.__class__ and self.content == other.content
-            
-    def __ne__(self, other) -> bool:
-        return self.__class__ != other.__class__ or self.content != other.content
-        
-    def __hash__(self):
-        return self == other
-
-    def __gt__(self, other):
-        return str(self) > str(other) 
-
-    def __ge__(self, other):
-        return self > other or self == other
-            
-    def __lt__(self, other):
-        return str(self) < str(other)
-    
-    def __le__(self, other):
-        return self < other or self == other
 
     @abstractmethod
     def decomposed(self, side) -> tuple[tupseq]:
@@ -54,38 +29,47 @@ class Proposition(ABC):
         Return results of decomposing an instance of the current
         proposition on input side.
         """
-        pass
+
+    @abstractmethod
+    def validate_content(self) -> None:
+        """Raises error if content has incorrect type."""
 
     class AtomicDecompositionError(Exception):
         def __init__(self, proposition):
             msg = f'{proposition} is an atom and cannot be decomposed.'
             super().__init__(msg)
 
-
+@dataclass(slots=True, frozen=True)
 class BinaryProposition(Proposition):
     """
     Super class for binary propositions.
     """
+    left: Proposition
+    right: Proposition
     arity = 2
-
-    def __init__(self, *args):
-        super().__init__(*args)
-        for arg in args:
-             if not isinstance(arg, Proposition):
-                 raise TypeError(f'{self.__class__} arguments '\
-                         f'must be Proposition not {type(arg)}')
 
     def __str__(self):
         return f'({self.content[0]} {self.symb} {self.content[1]})'
 
+    def validate_content(self) -> None:
+        for prop in self.content:
+            if not isinstance(prop, Proposition):
+                raise TypeError(
+                    f'{self.__class__} content requires propositions, not {type(prop)}.'
+                )
+
+    @property
+    def content(self) -> list[Proposition, Proposition]:
+        return [self.left, self.right]
+
+
+@dataclass(slots=True, frozen=True)
 class Atom(Proposition):
     """
     Proposition class with no logical content.
     """
+    prop: str
     arity = 1
-
-    def __init__(self, *args):
-        super().__init__(*args)
     
     def __str__(self) -> str:
         return f'{self.content[0]}'
@@ -94,24 +78,40 @@ class Atom(Proposition):
     def complexity(self) -> int:
         return 0
 
+    @property
+    def content(self) -> list[str]:
+        return [self.prop]
+
+    def validate_content(self) -> None:
+        if not isinstance(self.prop, str):
+            raise TypeError(
+                f'{self.__class__} content requires string, not {type(self.prop)}.'
+            )
+
     def decomposed(self, side) -> None:
         raise self.AtomicDecompositionError()
 
 
+@dataclass(slots=True, frozen=True)
 class Negation(Proposition):
     """
     Unary proposition signifying logical 'not ...'.
     """
+    negatum: Proposition
     arity = 1
-
-    def __init__(self, *args):
-        super().__init__(*args)
-        if not isinstance(args[0], Proposition):
-            raise TypeError('Negation content must be Proposition, '\
-                    f'not {type(args[0])}.')
 
     def __str__(self) -> str:
         return f'(~{self.content[0]})'
+
+    @property
+    def content(self) -> list[Proposition]:
+        return [self.negatum]
+
+    def validate_content(self) -> None:
+        if not isinstance(self.negatum, Proposition):
+            raise TypeError(
+                f'{self.__class__} content requires Proposition, not {type(self.negatum)}.'
+            )
 
     def decomposed(self, side) -> tuple[tupseq]:
         assert side in SIDES
@@ -119,14 +119,13 @@ class Negation(Proposition):
             return tupseq([], [self.content[0]]),
         return tupseq([self.content[0]], []),
 
+
+@dataclass(slots=True, frozen=True)
 class Conjunction(BinaryProposition):
     """
     Binary proposition signifying logical '... and ...'.
     """
     symb = '&'
-
-    def __init__(self, *args):
-        super().__init__(*args)
 
     def decomposed(self, side) -> tuple[tupseq]:
         assert side in SIDES
@@ -135,14 +134,12 @@ class Conjunction(BinaryProposition):
         return tupseq([], [self.content[0]]), tupseq([], [self.content[1]])
 
 
+@dataclass(slots=True, frozen=True)
 class Conditional(BinaryProposition):
     """
     Binary proposition signifying logical 'if ... then ...'
     """
     symb = '->'
-
-    def __init__(self, *args):
-        super().__init__(*args)
     
     def decomposed(self, side) -> tuple[tupseq]:
         assert side in SIDES
@@ -151,15 +148,13 @@ class Conditional(BinaryProposition):
         return tupseq([self.content[0]], [self.content[1]]),
 
 
+@dataclass(slots=True, frozen=True)
 class Disjunction(BinaryProposition):
     """
     Binary proposition signifying logical '... or ...'
     """
     symb = 'v'
 
-    def __init__(self, *args):
-        super().__init__(*args)
-    
     def decomposed(self, side) -> tuple[tupseq]:
         assert side in SIDES
         if side == 'ant':
