ca790ae677cbbfcf14e4d1f02eacb8a90bcab72c
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-08-23 19:20:48 -0400

    made tests for invertible rules resilient to settings changes

--- test/test_tree.py
+++ test/test_tree.py
@@ -37,31 +37,33 @@ class TestTree(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_tree_grows_one_parent_invertible_only(self) -> None:
-        sequent = Sequent((self.cj,), ())
-        tree = Tree(sequent)
-        tree.grow()
-        actual = tree.branches
-        expected = {
-            sequent: {
-                Sequent((self.p, self.q), ()): None
+        with patch('rules.get_rule_setting', return_value='mul'):
+            sequent = Sequent((self.cj,), ())
+            tree = Tree(sequent)
+            tree.grow()
+            actual = tree.branches
+            expected = {
+                sequent: {
+                    Sequent((self.p, self.q), ()): None
+                }
             }
-        }
-        self.assertEqual(expected, actual)
+            self.assertEqual(expected, actual)
 
     def test_tree_grows_two_parent_invertible_only(self) -> None:
-        sequent = Sequent((self.dj,), ())
-        tree = Tree(sequent)
-        tree.grow()
-        actual = tree.branches
-        expected = {
-            sequent: {
-                Sequent((self.p,), ()): None,
-                Sequent((self.q,), ()): None
-
+        with patch('rules.get_rule_setting', return_value='add'):
+            sequent = Sequent((self.dj,), ())
+            tree = Tree(sequent)
+            tree.grow()
+            actual = tree.branches
+            expected = {
+                sequent: {
+                    Sequent((self.p,), ()): None,
+                    Sequent((self.q,), ()): None
+    
+                }
             }
-        }
-        self.assertEqual(expected, actual)
-
+            self.assertEqual(expected, actual)
+    
     def test_tree_can_only_be_grown_once(self) -> None:
         sequent = Sequent((), ())
         tree = Tree(sequent)
