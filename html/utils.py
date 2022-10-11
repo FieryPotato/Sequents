0f73d6d7e72013dfd30453f7ae45a89088109323
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-10-10 22:35:38 -0400

    First level of gridify passes.

--- html/utils.py
+++ html/utils.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from typing import Protocol
 
 
@@ -12,16 +10,23 @@ class Tree(Protocol):
     def width(self) -> int:
         ...
 
+ARRAY_VALS = {
+    str: '',
+    None: None,
+    int: 0,
+    'css': '.'
+}
 
-def get_array(tree: Tree) -> np.chararray:
+def get_array(tree: Tree, dtype=None) -> list[list[str]]:
     """
-    Return a 2d numpy array of strings whose dimensions will fit tree.
+    Return a 2d list of strings whose dimensions will fit tree. dtype
+    is either a builtin type (str, None, or int) or 'css', which fills
+    the cell with "'.'".
     """
     rows = 1 + (2 * tree.height())
     columns = 2 * tree.width()
-    result = np.chararray(shape=(rows, columns))
-    result.fill(b'')
-    return result
+    val = ARRAY_VALS[dtype]
+    return [[val for _ in range(columns)] for _ in range(rows)]
 
 
 def gridify(tree: Tree) -> tuple[list, list]:
@@ -29,7 +34,23 @@ def gridify(tree: Tree) -> tuple[list, list]:
     Return a pair of lists of lists which represent grids in which a
     tree's branches are embedded.
     """
-    base = get_array(tree)
-    css: np.chararray = base.copy()
-    objects: np.chararray = base.copy()
+    css = get_array(tree, dtype='css')
+    objects = get_array(tree, dtype=None)
+    for sequent, parents in tree.branches.items():
+        # place tags
+        css[1][len(css[0]) - 1] = 'ft'
+        css[2][len(css[0]) - 1] = 'ft'
+        objects[1][len(objects[0]) - 1] = sequent.tag()
+        objects[2][len(objects[0]) - 1] = sequent.tag()
+
+        # place objects
+        for i in range(len(css[0]) - 1):
+            css[0][i] = 'f'
+            css[1][i] = 'f'
+            objects[0][i] = str(sequent)
+            objects[1][i] = str(sequent)
+    css.reverse()
+    objects.reverse()
+    return css, objects
+        
 
--- test/test_html.py
+++ test/test_html.py
@@ -7,121 +7,121 @@ from html.utils import get_array, gridify
 class TestHTMLArrayCreation(unittest.TestCase):
     def test_atom(self) -> None:
         t = string_to_tree('A; B')
-        actual = get_array(t).tolist()
+        actual = get_array(t, dtype=str)
         expected = [
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
         ]
         self.assertEqual(expected, actual)
 
     def test_complexity_1(self) -> None:
         t_1 = string_to_tree('A & B; C')
-        a_1 = get_array(t_1).tolist()
+        a_1 = get_array(t_1, dtype=str)
 
         e_1 = [
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
         ]
         self.assertEqual(e_1, a_1)
 
         t_2 = string_to_tree('A; B & C')
-        a_2 = get_array(t_2).tolist()
+        a_2 = get_array(t_2, dtype=str)
 
         e_2 = [
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
         ]
         self.assertEqual(e_2, a_2)
 
     def test_complexity_2(self) -> None:
         t_1 = string_to_tree('A; B -> (C -> D)')
-        a_1 = get_array(t_1).tolist()
+        a_1 = get_array(t_1, dtype=str)
 
         e_1 = [
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
-            [b'', b''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
+            ['', ''],
         ]
         self.assertEqual(e_1, a_1)
 
         t_2 = string_to_tree('; A & (B v C)')
-        a_2 = get_array(t_2).tolist()
+        a_2 = get_array(t_2, dtype=str)
 
         e_2 = [
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
         ]
         self.assertEqual(e_2, a_2)
 
         t_5 = string_to_tree('; A v (B & C)')
-        a_5 = get_array(t_5).tolist()
+        a_5 = get_array(t_5, dtype=str)
 
         e_5 = [
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
-            [b'', b'', b'', b''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
         ]
         self.assertEqual(e_5, a_5)
 
         t_6 = string_to_tree('(A v B) v C; ')
-        a_6 = get_array(t_6).tolist()
+        a_6 = get_array(t_6, dtype=str)
 
         e_6 = [
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
         ]
         self.assertEqual(e_6, a_6)
 
         t_7 = string_to_tree('; A & (B & C)')
-        a_7 = get_array(t_7).tolist()
+        a_7 = get_array(t_7, dtype=str)
 
         e_7 = [
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
+            ['', '', '', '', '', ''],
         ]
         self.assertEqual(e_7, a_7)
 
         t_8 = string_to_tree('A v B; C & D')
-        a_8 = get_array(t_8).tolist()
+        a_8 = get_array(t_8, dtype=str)
 
         e_8 = [
-            [b'', b'', b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b'', b'', b''],
-            [b'', b'', b'', b'', b'', b'', b'', b'']
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', '']
         ]
         self.assertEqual(e_8, a_8)
 
@@ -130,16 +130,16 @@ class TestGridification(unittest.TestCase):
     def test_one(self) -> None:
         tree = string_to_tree('A; B')
         expected_css = [
-            ['', 'ft'],
-            ['', 'ft'],
-            ['', '' ],
+            ['.', 'ft'],
+            ['f', 'ft'],
+            ['f', '.' ],
         ]
 
         seq = string_to_sequent('A; B')
         expected_objects = [
             [None, seq.tag()],
-            [seq,  seq.tag()],
-            [seq,  None]
+            [str(seq),  seq.tag()],
+            [str(seq),  None]
         ]
         expected = expected_css, expected_objects
         actual = gridify(tree)
