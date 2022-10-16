cc92ec0629d226e760f657e9e620ee30ab95710e
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-10-16 00:58:42 -0400

    refactoring

--- html/utils.py
+++ html/utils.py
@@ -10,12 +10,20 @@ class Tree(Protocol):
     def width(self) -> int:
         ...
 
+
 ARRAY_VALS = {
     str: '',
     None: None,
     int: 0,
     'css': '.'
 }
+
+TAG_VALS = {
+    0: 'm',
+    1: 'l',
+    2: 'r'
+}
+
 
 def get_array(tree: Tree, dtype=None) -> list[list[str]]:
     """
@@ -39,7 +47,7 @@ def gridify(tree: Tree) -> tuple[list, list]:
     objects = get_array(tree, dtype=None)
 
     root, branches = next(iter(tree.branches.items()))
-    
+
     # place root tags
     css[1][-1] = 'ft'
     css[2][-1] = 'ft'
@@ -53,49 +61,52 @@ def gridify(tree: Tree) -> tuple[list, list]:
         objects[1][i] = str(root)
 
     if branches is not None:
-        css, objects = gridify_branch(branches, css, objects, 
-            x=0, y=2)       
+        css, objects = gridify_branch(branches, css, objects)
     css.reverse()
     objects.reverse()
     return css, objects
-        
-def gridify_branch(branch: dict, css: list, objects: list,
-        x: int = 0, y: int = 0, tag: str = 'f') -> tuple[list, list]:
+
+
+def gridify_branch(branch: dict, css: list, objects: list, tag: str = 'f') -> tuple[list, list]:
     """Do the gridify work."""
-    mymap = {
-        0: 'm',
-        1: 'l',
-        2: 'r'
-    }
-    for i, items in enumerate(branch.items()):
-        sequent, parents = items
-        if len(branch.values()) == 1:
-            # place objects
-            css[2][0] = tag + 'm'
-            css[3][0] = tag + 'm'
-            objects[2][0] = str(sequent)
-            objects[3][0] = str(sequent)
-
-            # place tags
-            css[3][-1] = tag + 'mt'
-            css[4][-1] = tag + 'mt'
-            objects[3][-1] = sequent.tag()
-            objects[4][-1] = sequent.tag()
-
-        elif len(branch.values()) == 2:
-            j = i + 1
-
-            # place objects
-            css[2][2 * i] = tag + mymap[j]
-            css[3][2 * i] = tag + mymap[j]
-            objects[2][2 * i] = str(sequent)
-            objects[3][2 * i] = str(sequent)
-
-            # place tags
-            css[3][2 * j - 1] = tag + mymap[j] + 't'
-            css[4][2 * j - 1] = tag + mymap[j] + 't'
-            objects[3][2 * j - 1] = sequent.tag()
-            objects[4][2 * j - 1] = sequent.tag()
+    match len(branch.values()):
+        case 1:
+            gridify_one_parent_branch(branch, css, objects, tag)
+        case 2:
+            gridify_two_parent_branch(branch, css, objects, tag)
 
     return css, objects
 
+
+def gridify_two_parent_branch(branch, css, objects, tag):
+    for i, items in enumerate(branch.items()):
+        sequent, parents = items
+        j = i + 1
+
+        # place objects
+        css[2][2 * i] = tag + TAG_VALS[j]
+        css[3][2 * i] = tag + TAG_VALS[j]
+        objects[2][2 * i] = str(sequent)
+        objects[3][2 * i] = str(sequent)
+
+        # place tags
+        css[3][2 * j - 1] = tag + TAG_VALS[j] + 't'
+        css[4][2 * j - 1] = tag + TAG_VALS[j] + 't'
+        objects[3][2 * j - 1] = sequent.tag()
+        objects[4][2 * j - 1] = sequent.tag()
+
+
+def gridify_one_parent_branch(branch, css, objects, tag):
+    sequent = next(iter(branch.keys()))
+
+    # place objects
+    css[2][0] = tag + 'm'
+    css[3][0] = tag + 'm'
+    objects[2][0] = str(sequent)
+    objects[3][0] = str(sequent)
+
+    # place tags
+    css[3][-1] = tag + 'mt'
+    css[4][-1] = tag + 'mt'
+    objects[3][-1] = sequent.tag()
+    objects[4][-1] = sequent.tag()
