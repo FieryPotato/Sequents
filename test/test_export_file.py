979d326703af84cb99da4756dad1358f9332840c
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-10-30 14:43:33 -0400

    Collisions are handled by overwriting data

--- test/test_export_file.py
+++ test/test_export_file.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import pickle
 from pathlib import Path
@@ -6,7 +7,12 @@ from convert import string_to_tree
 from export_file import PickleExporter
 
 
-class TestExportFile(unittest.TestCase):
+T_0 = string_to_tree('A; B -> C')
+T_1 = string_to_tree('A v B; C & (D v E)')
+TREE_LIST = [T_0, T_1]
+
+
+class TestExportBytes(unittest.TestCase):
     dir: str = 'test/io_testing/export/'
     bytes_out_w_dir: str = 'test/io_testing/export/results.sequents'
     bytes_out_w_file: str = 'test/io_testing/export/export.sequents'
@@ -25,33 +31,37 @@ class TestExportFile(unittest.TestCase):
         test_dir.rmdir()
 
     def test_saving_tree_to_dir_bytes(self) -> None:
-        t_0 = string_to_tree('A; B -> C')
-
-        t_1 = string_to_tree('A v B; C & (D v E)')
-        tree_list = [t_0, t_1]
-                        
         exporter = PickleExporter(self.dir)
-        exporter.export(tree_list)
+        exporter.export(TREE_LIST)
 
         with open(self.bytes_out_w_dir, 'rb') as f:
             actual = pickle.load(f)
         
-        self.assertEqual(tree_list, actual)
+        self.assertEqual(TREE_LIST, actual)
 
     def test_saving_tree_to_file_bytes(self) -> None:
-        t_0 = string_to_tree('A; B -> C')
-
-        t_1 = string_to_tree('A v B; C & (D v E)')
-        tree_list = [t_0, t_1]
-
         exporter = PickleExporter(self.dir + 'export.sequents')
-        exporter.export(tree_list)
+        exporter.export(TREE_LIST)
 
         with open(self.bytes_out_w_file, 'rb') as f:
             actual = pickle.load(f)
 
-        self.assertEqual(tree_list, actual)
+        self.assertEqual(TREE_LIST, actual)
 
+    def test_handling_collisions(self) -> None:
+        # Collisions are handled by overwriting previous data.
+        pre_existing = Path(self.bytes_out_w_dir)
+        os.makedirs(parent := pre_existing.parent)
+        pre_existing.touch()
+
+        exporter = PickleExporter(self.dir)
+        exporter.export(TREE_LIST)
+
+        new_path = parent / 'results.sequents'
+        with open(new_path, 'rb') as f:
+            actual = pickle.load(f)
+
+        self.assertEqual(TREE_LIST, actual)
 
 if __name__ == '__main__':
     unittest.main()
