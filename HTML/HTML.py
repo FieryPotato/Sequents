91d236cd5a7076adcde13ce77e7351360dc90e95
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-11-02 14:17:01 -0400

    HTML head is created with stylesheet

--- HTML/HTML.py
+++ HTML/HTML.py
@@ -1,9 +1,27 @@
+import json
+from pathlib import Path
+
 import dominate
 
 from dominate import tags
 
 
 class HTML:
+    tree_class = ".tree {" \
+                 "display: grid;" \
+                 "justify-content: center;" \
+                 "align-items: center;" \
+                 "margin: 10px;" \
+                 "padding: 10px;" \
+                 "}"
+    cell_class = ".cell {" \
+                 "text-align: center;" \
+                 "border-top: 2px solid black;" \
+                 "padding: 0px 0px 0px 10px;" \
+                 "margin: 0px 4px 0px 16px;" \
+                 "}"
+    classes = tree_class, cell_class
+
     def __init__(self, out_path, title='') -> None:
         self.path = out_path
         self.file = dominate.document(title=title)
@@ -17,3 +35,19 @@ class HTML:
     def save(self) -> None:
         with open(self.path, 'w') as f:
             f.write(self.file.render())
+
+    def create_head(self) -> None:
+        with self.file.head:
+            # start stylesheet formatting
+            stylesheet = ['\n']
+            # add class stylings
+            stylesheet.extend(
+                    ' ' * 6 + css_class + '\n' for css_class in self.classes
+            )
+            # end stylesheet formatting
+            stylesheet.append(' ' * 4)
+            # typeset stylesheet
+            tags.style(stylesheet)
+
+    def create_table_of_contents(self) -> None:
+        pass
